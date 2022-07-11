0e0d91c4debea37aa4cf63258de61d6d6b422ede
Joshbam-joshwilly <Alanajoshua535@gmail.comgit config --global user.name Joshbam-joshwillygit config --global user.email Alanajoshua535@gmail.comgit config --global user.email Alanajoshua535@gmail.com>
2022-07-11 14:57:15 +0100

    the card magic code

---
+++ magnet.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+
+{
+  char card_name[3];
+  puts("Enter the card_name");
+  scanf("%2s", card_name);
+
+  int val = 0;
+
+  if(card_name[0] == 'K')
+    {
+      val = 10;
+    } else if(card_name[0] == 'Q')
+    {
+      val = 10;
+    } else if(card_name[0] == 'J')
+    {
+      val = 10;
+    } else if(card_name[0] == 'A')
+    {
+      val = 10;
+    } else {
+    val = atoi(card_name);
+  }
+  printf("The card value is: %i\n", val);
+  return 0;
+
+}
